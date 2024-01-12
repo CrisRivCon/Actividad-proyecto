61756e8add8f0c5cc4823b200585cd58628aff05
Cristina Rivera <cristinapruebas14@gmail.com>
2024-01-12 17:14:24 +0100

    Tab en proceso

---
+++ script/script.js
@@ -0,0 +1,11 @@
+let selectores = document.querySelectorAll('.selectores div');
+
+for (i = 0; i < selectores.length; i++) {
+    selectores[i].addEventListener('click', e => {
+        cambioDeTab(e);
+    });
+}
+
+function cambioDeTab() {
+    
+}
