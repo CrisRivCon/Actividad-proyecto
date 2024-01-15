f799bfcb4aede79cde8f86536174a38304672562
Cris wind <cristinapruebas14@gmail.com>
2024-01-15 16:11:49 +0100

    Tabs

--- script/script.js
+++ script/script.js
@@ -1,11 +1,27 @@
 let selectores = document.querySelectorAll('.selectores div');
+const TAB = {
+    'socio-tab': 'socio',
+    'micro-tab': 'micro',
+    'dona-tab': 'dona',
+    'apadrina-tab': 'apadrina',
+}
 
 for (i = 0; i < selectores.length; i++) {
     selectores[i].addEventListener('click', e => {
         cambioDeTab(e);
+        cambioDescripcion(e);
     });
 }
 
-function cambioDeTab() {
-    
+function cambioDeTab(e) {
+    let activo = document.querySelector('.tab-activo');
+    activo.classList.remove('tab-activo');
+    e.target.classList.add('tab-activo');
+}
+
+function cambioDescripcion(e) {
+    let descripcion = document.getElementById(TAB[e.target.id]);
+    let descripcionActiva = document.querySelector('.activo');
+    descripcionActiva.classList.remove('activo');
+    descripcion.classList.add('activo');
 }
