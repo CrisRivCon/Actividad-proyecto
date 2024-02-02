3efc5768fa40c6b367bbf7ab215c909df494c35b
Cristina Rivera <cristinapruebas14@gmail.com>
2024-02-02 17:14:33 +0100

    Colores accesibles en Contacto y Blog

--- script/script.js
+++ script/script.js
@@ -10,65 +10,47 @@ function mostrarMenu() {
 };
 
 // Cambio de colores
-let accesible = false;
 const linkContrasteAlto = document.getElementById('tema-accesible');
 const linkColorClasico = document.getElementById('tema-clasico');
 
 const cuerpo = document.getElementById('body');
-console.log(cuerpo);
-
-const tituloSeccion2 = document.querySelector('.texto-secc-2>h2');
-const fondoSeccion2 = document.querySelector('.fondo-seccion-2');
-const tooltip = document.querySelector('.tooltip');
-const botonesPrimarios = document.querySelectorAll('.boton-primario');
-const botonesSecundarios = document.querySelectorAll('.boton-secundario');
 
 linkColorClasico.addEventListener('click', () => {
-    accesible = false;
-    document.cookie = 'accesible = false';
+    borrarCuki('accesible');
+    //document.cookie = 'accesible = false';
+    setCookie('accesible', false);
     colorClasico();
 });
 
 linkContrasteAlto.addEventListener('click', () => {
-    accesible = true;
-    document.cookie = 'accesible = true';
+    borrarCuki('accesible');
+    setCookie('accesible', true);
+    //document.cookie = 'accesible = true';
     colorAccesible();
 });
 
 // Comprobar accesible
 let cuki = document.cookie;
+console.log(cuki);
 let esAccesible = cuki.split('=')[1];
 esAccesible == 'true' ? colorAccesible() : colorClasico();
 
 function colorAccesible() {
     cuerpo.classList.add('accesible');
-
- /*    fondoSeccion2.classList.add('fondo-seccion-2-accesible');
-    tituloSeccion2.classList.add('titulo-texto-acc');
-    tooltip.classList.add('tooltip-acc');
-    botonesPrimarios.forEach(boton => {
-        boton.classList.add('boton-primario-acc');
-    });
-    botonesSecundarios.forEach(boton => {
-        boton.classList.add('boton-secundario-acc');
-    });
-    seccion3.classList.add('seccion-3-acc');
- */
 }
 
 function colorClasico() {
     cuerpo.classList.remove('accesible');
-
-   /*  fondoSeccion2.classList.remove('fondo-seccion-2-accesible');
-    tituloSeccion2.classList.remove('titulo-texto-acc');
-    tooltip.classList.remove('tooltip-acc');
-    botonesPrimarios.forEach(boton => {
-        boton.classList.remove('boton-primario-acc');
-    });
-    botonesSecundarios.forEach(boton => {
-        boton.classList.remove('boton-secundario-acc');
-    });
-    seccion3.classList.remove('seccion-3-acc'); */
 }
 
 
+function setCookie(nombre, valor, expires, path = '/') {
+    let cuki = encodeURIComponent(nombre) + '=' + encodeURIComponent(valor) + '; path =' + path;
+    let fecha = new Date(Date.now() + (expires * 24 * 60 * 60));
+    document.cookie = cuki + ';expires=' + fecha.toUTCString() + '; samesite = lax';
+}
+
+function borrarCuki(nombre) {
+    setCookie(nombre, '', -10);
+
+}
